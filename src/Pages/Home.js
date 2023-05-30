796e698f9f3b3031cfa7a4bfe4b5a638586fc567
chenshy25 <1712823010@qq.com>
2023-05-30 09:46:34 +0800

    article

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,17 @@
 /**
  * @author {Jingyun Huang}
  * @date 2023/5/24
+ * @author {Shiyu Chen}
+ * @date 2023/5/28
  */
 
 import React from "react";
 import {
-    Box, Text, useColorModeValue, Heading, VStack,
+    Box, Text, useColorModeValue, Heading, VStack, Link
 
 } from "@chakra-ui/react";
+
+import { ExternalLinkIcon } from '@chakra-ui/icons';
 
 const HomePage = (props) => {
     const borderColor = useColorModeValue('gray.200', 'gray.600')
@@ -17,10 +21,12 @@ const HomePage = (props) => {
         {
             title: "Exploring the Wonders of Nature",
             content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae odio a dui ultricies tincidunt. Curabitur dignissim lectus in dui aliquam, sed rutrum mauris eleifend. Vivamus commodo, ipsum in fermentum scelerisque, velit lacus malesuada risus, at sagittis diam lectus nec nisi.",
+            location: "http://localhost:3000/article1"
         },
         {
             title: "The Art of Balancing Work and Life",
             content: "Fusce fermentum metus non mauris consequat, ac aliquam mauris pulvinar. Morbi sed justo eget justo feugiat congue. Vivamus elementum ligula vel turpis convallis, non vulputate tellus eleifend. Quisque quis sapien ligula. Donec commodo quam non interdum facilisis.",
+            location: "http://localhost:3000/article2"
         },
     ];
 
@@ -42,9 +48,11 @@ const HomePage = (props) => {
                 <VStack spacing="6" align="start">
                     {articles.map((article, index) => (
                         <Box key={index} borderWidth="1px" p="4" borderRadius="lg">
+                            <Link color='blackAlpha.900' href = {article.location} >
                             <Heading as="h2" size="lg" mb="2">
                                 {article.title}
                             </Heading>
+                            </Link>
                             <Text>{article.content}</Text>
                         </Box>
                     ))}
