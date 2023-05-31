9a8d9e0da6b7b145d35aa2ca9cde3b01d34ce3c5
nishj-sj <1281573703@qq.com>
2023-05-31 17:32:58 +0800

    添加数据库表

--- server/BLOG/models.py
+++ server/BLOG/models.py
@@ -7,4 +7,13 @@ class EventTable(models.Model):
     name = models.CharField(max_length=20)
     category = models.CharField(max_length=10)
     url = models.CharField(max_length=50)
+class Atricle(models.Model):
+    #Django自动设置id为自增主键
+    ##id = models.AutoField(primary_key=True) #自增id=>主键
+    title = models.CharField(max_length=50) 
+    date = models.DateField(auto_now_add=True)
+    url = models.CharField(max_length=50)
+class Tag(models.Model):
+    article_id = models.IntegerField()
+    tag = models.CharField(max_length=20)
 # Create your models here.
